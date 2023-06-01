0f1e11453d84954050b6ee820debdf6c3d28ff63
luccronin <l.cronin@student.rug.nl>
2023-06-01 15:47:52 +0200

    Wrote a function to go through the csv file containing the Mv readings and apply a butterworth filter.

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.signal import butter, filtfilt
+
+
+def buter(mv_file):
+    """
+    This function implements a butterworth filter with
+    an order of 3,
+    a low cut-off of 0.4Hz,
+    a high cut-off of 45Hz,
+    a sampling rate of 360Hz.
+    :return: The filtered data.
+    """
+    order = 3
+    low_cut = 0.4
+    high_cut = 45
+    sampling_rate = 360
+    nyq = 0.5 * sampling_rate
+    low = low_cut / nyq
+    high = high_cut / nyq
+    coef = butter(order, [low, high], btype='band')
+    a = coef[0]
+    b = coef[1]
+
+    df = pd.read_csv(mv_file)
+    index = np.asarray(df.iloc[:, 0])
+    print(index.shape)
+    mv_readings = np.asarray(df.iloc[:, 1:])
+    filtered_data = filtfilt(a, b, mv_readings, axis=0)
+    filtered_df = np.insert(filtered_data, 0, index, axis=1)
+
+    return filtered_df
 
 
 # Future Notes:
@@ -13,12 +44,12 @@ def read_in_csv(mv_file, annotations_file):
     This function reads in 2 csv files and returns 1 dataframe.
     :param mv_file: The csv file containing the mv readings for the whole ambulatory ECG scan.
     :param annotations_file: The csv file containing the annotations for the ambulatory ECG scan.
-    :return: The returned data frame consists of the
-    timeseries of a heartbeat, followed by the annotations of the heartbeat. The time series consists of a list of
-    tuples with two values, one for each channel.
+    :return: The returned data frame consists of [sample_index][mv_readings = 0|annotation = 1][Channel 1 or 2|Type] &
+    the types of heartbeats in a list.
     """
     # mv_readings structure: #sample, Signal reading 1, Signal reading 2
     # there are 650k samples.
+    buttery_df = buter(mv_file)
     mv_readings = pd.read_csv(mv_file)
 
     # annotations structure: Time   Sample #  Type  Sub Chan  Num
@@ -36,17 +67,19 @@ def read_in_csv(mv_file, annotations_file):
         sample_row_x = []  # This is the time series of a heartbeat
         sample_row_y = []  # This is the annotation of the heartbeat
         upper_bound = annotations.iloc[i]['Sample']
+        # if we see a type of heartbeat that we have not seen before, we add it to the list of heartbeat types
+        if annotations.iloc[i]['Type'] not in heartbeat_types:
+            heartbeat_types.append(annotations.iloc[i]['Type'])
+
+        sample_row_y.append(annotations.iloc[i]['Type'])
+
+        # Run through the mv readings and append the readings to the sample_row_x as tuples of (channel 1, channel 2)
         for j in range(lower_bound, upper_bound):
-            sample_row_x.append((mv_readings.iloc[j][1], mv_readings.iloc[j][2]))
-            if j == upper_bound - 1:
-                sample_row_y.append(annotations.iloc[i]['Type'])
-                if annotations.iloc[i]['Type'] not in heartbeat_types:
-                    heartbeat_types.append(annotations.iloc[i]['Type'])
+            sample_row_x.append((buttery_df[j][1], buttery_df[j][2]))
 
         train_data.append([sample_row_x, sample_row_y])
         lower_bound = upper_bound + 1
 
-    print(f'the training data is currently {train_data[1]}')
     return train_data, heartbeat_types
 
 
@@ -65,7 +98,7 @@ def split_into_types(data, heartbeat_types):
     for i in range(len(data)):
         beat = data[i][1][0]
         if beat in heartbeat_types:
-            classes[heartbeat_types.index(beat)].append(data[i][0])
+            classes[heartbeat_types.index(beat)].append(data[i])
 
     return classes
 
@@ -107,13 +140,12 @@ def plot_class_distribution(plot_data, heartbeat_types):
 def main():
     mv_file = '201.csv'
     annotation_file = '201annotations.csv'
-
     train_data, heartbeat_types = read_in_csv(mv_file, annotation_file)
 
-    # plot_data = count_classe_instances(train_data, heartbeat_types)
-    other_data = split_into_types(train_data, heartbeat_types)
-
-    print(other_data[0][0])
+    plot_data = count_classe_instances(train_data, heartbeat_types)
+    # This matrix contains as many lists as there are types of heartbeats. Each list then has every sample of that type.
+    class_split_df = split_into_types(train_data, heartbeat_types)
+    plot_class_distribution(plot_data, heartbeat_types)
 
 
 if __name__ == '__main__':
