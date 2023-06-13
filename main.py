54ad06eb486a1f9b79febacac95ca35f98cef8e7
luccronin <l.cronin@student.rug.nl>
2023-06-13 23:17:10 +0200

    Updated some functions. Got the matrices for the ESN model I think complete.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import butter, filtfilt
+import test
 
 
 def buter(mv_file):
@@ -11,7 +12,7 @@ def buter(mv_file):
     a low cut-off of 0.4Hz,
     a high cut-off of 45Hz,
     a sampling rate of 360Hz.
-    :return: The filtered data.
+    :return: The filtered data in a numpy array.
     """
     order = 3
     low_cut = 0.4
@@ -26,7 +27,6 @@ def buter(mv_file):
 
     df = pd.read_csv(mv_file)
     index = np.asarray(df.iloc[:, 0])
-    print(index.shape)
     mv_readings = np.asarray(df.iloc[:, 1:])
     filtered_data = filtfilt(a, b, mv_readings, axis=0)
     filtered_df = np.insert(filtered_data, 0, index, axis=1)
@@ -39,17 +39,19 @@ def buter(mv_file):
 # 2. The channel readings are not float point numbers, this should be changed to ensure no loss of info.
 # 3. All of these are lists and will first need to be converted to numpy arrays before appedning to the train_data list.
 # 4. The training data will also need to be converted into a numpy array before being fed into the model.
-def read_in_csv(mv_file, annotations_file):
+def old_read_in_csv(mv_file, annotations_file):
     """
     This function reads in 2 csv files and returns 1 dataframe.
     :param mv_file: The csv file containing the mv readings for the whole ambulatory ECG scan.
     :param annotations_file: The csv file containing the annotations for the ambulatory ECG scan.
-    :return: The returned data frame consists of [sample_index][mv_readings = 0|annotation = 1][Channel 1 or 2|Type] &
-    the types of heartbeats in a list.
+    :return: The dataframe containing the mv readings and the annotations as a numpy array & a list of the types.
+    Data Frame Structure:
+    The returned data frame consists of [sample_index][mv_readings = 0|annotation = 1][tuples of channels]
+    [Channel 1 or 2|Type] & the types of heartbeats in a list.
     """
     # mv_readings structure: #sample, Signal reading 1, Signal reading 2
     # there are 650k samples.
-    buttery_df = buter(mv_file)
+    buttery_df = buter(mv_file)  # This is the filtered data, using the butterworth filter.
     mv_readings = pd.read_csv(mv_file)
 
     # annotations structure: Time   Sample #  Type  Sub Chan  Num
@@ -81,6 +83,76 @@ def read_in_csv(mv_file, annotations_file):
         lower_bound = upper_bound + 1
 
     return train_data, heartbeat_types
+
+
+def read_in_csv(mv_file, annotations_file):
+    # So this currently is a numpy matrix with shape (650_000, 3)
+    butter_df = buter(mv_file)
+
+    # annotations structure: Time   Sample #  Type  Sub Chan  Num
+    # there are 2275 annotations.
+    annotations_cols = ['Time', 'Sample', 'Type', 'Sub', 'Chan', 'Num']
+    annotations = pd.read_csv(annotations_file, usecols=annotations_cols)
+    # the last 3 columns are not needed, so we need a new df with only the first 3 columns
+    # The shape of the annotations is (2_039, 3)
+    annotations = annotations.iloc[:, :3]
+
+    print(annotations.iloc[0])
+
+    train_data = []
+    label_data = []
+    heartbeat_types = []
+
+    upper_bound = 0
+    lower_bound = 0
+    for i in range(annotations.shape[0]):
+        if annotations.iloc[i][0] not in heartbeat_types:
+            heartbeat_types.append(annotations.iloc[i][2])
+
+        label_data.append(annotations.iloc[i][2])
+
+        heartbeat_sequence = []
+        upper_bound = annotations.iloc[i][1]
+        for pair in range(lower_bound, upper_bound):  # The value at the upper bound is not included.
+            # inspect later if converting the pair into a np array is necessary.
+            channel_readings = [butter_df[pair][1], butter_df[pair][2]]
+            heartbeat_sequence.append(np.asarray(channel_readings))
+
+        train_data.append(np.asarray(heartbeat_sequence))
+        lower_bound = upper_bound + 1
+
+    return train_data, label_data, heartbeat_types
+
+
+def compare(new_seq, label, old):
+    """
+    This is just a quick helper function, wanted to see if the way I structured the data was correct and that all values
+    matched up as well as labels
+    :param new_seq: The new sequence of heartbeats, this is now only the training data input.
+    :param label: This is target labels for the input data.
+    :param old: This is the old data structure that was used where the input and label were both present.
+    :return:
+    """
+    if len(new_seq) != len(label):
+        raise ValueError("The two lists are not the same length.")
+
+    print(new_seq[0][0][0])
+    print('------')
+    print(old[0][0][0][0])
+
+    # This layer will get the heartbeat segments
+    for i in range(len(new_seq)):
+        # this layer will iterate though the values in the segment
+        for j in range(len(old[i])):
+            if new_seq[i][j][0] != old[i][0][j][0] and new_seq[i][j][1] != old[i][0][j][1]:
+                print("The values do not match.")
+                return False
+            # check if the label matches the label in the old list
+            if label[i] != old[i][1][0]:
+                print("The labels do not match.")
+                return False
+
+    print("The values match and labels match.")
 
 
 def split_into_types(data, heartbeat_types):
@@ -137,15 +209,74 @@ def plot_class_distribution(plot_data, heartbeat_types):
     plt.show()
 
 
+def get_avg_seq_length(train_data):
+    """
+    This function calculates the average length of the sequences in the training data.
+    :param train_data: The training data.
+    :return: The average length of the sequences in the training data.
+    """
+    # Calculate the number of sequences in the training data.
+    num_sequences = len(train_data)
+    # Calculate the total length of the sequences in the training data.
+    total_length = 0
+    for sequence in train_data:
+        total_length += len(sequence[0])
+    # Calculate the average length of the sequences in the training data.
+    avg_seq_length = total_length / num_sequences
+    # Return the average length of the sequences in the training data.
+    return avg_seq_length
+
+
 def main():
     mv_file = '201.csv'
     annotation_file = '201annotations.csv'
-    train_data, heartbeat_types = read_in_csv(mv_file, annotation_file)
+    # the old read in csv function has the target labels in it as well.
+    train_data, heartbeat_types = old_read_in_csv(mv_file, annotation_file)
+    # The new one does not have the target labels in it and everything is a numpy array.
+    train_data, label_data, heartbeat_types = read_in_csv(mv_file, annotation_file)
 
-    plot_data = count_classe_instances(train_data, heartbeat_types)
+    # print(len(heartbeat_types))
+    # print(train_data[0][0][0])
+    # print(f'The average heartbeat segment length is: {get_avg_seq_length(train_data)}')
+
+    # plot_data = count_classe_instances(train_data, heartbeat_types)
     # This matrix contains as many lists as there are types of heartbeats. Each list then has every sample of that type.
     class_split_df = split_into_types(train_data, heartbeat_types)
-    plot_class_distribution(plot_data, heartbeat_types)
+    # plot_class_distribution(plot_data, heartbeat_types)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # This is the portion of code for building the ESN model.
+
+    Nx = 500
+    Nu = 2
+    Ny = 10
+    alpha = 0.3
+    sparseness = 0.1
+    little_bound = -1
+    big_bound = 1
+    # rescale_factor = 0.4
+
+    # create the input weight matrix w_in
+    w_in = test.generate_sparse_weights(Nx, Nu, sparseness, little_bound, big_bound)
+
+    # Generate the sparse matrix of weights for the reservoir (W).
+    sparse_matrix = test.generate_hidden_matrix(Nx, sparseness, little_bound, big_bound)
+    # Calculate the spectral radius of the reservoir weights.
+    spectral_radius = test.get_spectral_radius(sparse_matrix)
+    # rescale the weights
+    w = sparse_matrix / spectral_radius
+
+    # Generate the initial state of the reservoir.
+    x = test.generate_neurons(Nx)
+
+    # Generate the weights for the output layer of the ESN model.
+    w_out = test.generate_sparse_weights(Nx, Ny, sparseness, little_bound, big_bound)
+
+    # Generate the units for the output layer of the ESN model.
+    y = test.generate_neurons(Ny)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # This is the portion of code for training the ESN model.
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -2,15 +2,17 @@ import tensorflow as tf
 import numpy as np
 
 
-# The defining global parameters for the reservoir of the ESN model.
-# Nx : The number of neurons in the reservoir.
-# Sparsity : The percentage of neurons that are connected to other neurons.
-# Distribution pf non-zero weights : The distribution of the non-zero weights in the reservoir.
-# Spectral radius : The spectral radius of the reservoir (W).
-# The spectral radius is the largest absolute eigenvalue of a matrix.
-# The spectral radius is calculated after the reservoir weights W have been generated sparsely.
-# Input scaling : The scaling of the input weights (W-in).
-# Leaking rate : The leaking rate of the reservoir neurons.
+"""
+The defining global parameters for the reservoir of the ESN model.
+Nx : The number of neurons in the reservoir.
+Sparsity : The percentage of neurons that are connected to other neurons.
+Distribution pf non-zero weights : The distribution of the non-zero weights in the reservoir.
+Spectral radius : The spectral radius of the reservoir (W).
+The spectral radius is the largest absolute eigenvalue of a matrix.
+The spectral radius is calculated after the reservoir weights W have been generated sparsely.
+Input scaling : The scaling of the input weights (W-in).
+Leaking rate : The leaking rate of the reservoir neurons.
+"""
 
 
 # spectral radius is calculated after the reservoir weights W have been generated sparsely. So we will create a method
@@ -26,7 +28,21 @@ def get_spectral_radius(W):
     return np.max(np.abs(e))
 
 
-def generate_sparse_matrix(n_x, sparsity, lower_bound=-1, upper_bound=1):
+def generate_random_indices(matrix, n):
+    """
+    This function generates a random list of indices for a 2D matrix.
+    :param matrix: The matrix to generate the indices for.
+    :param n: The number of indices to generate.
+    :return: The list of indices.
+    """
+    rows, cols = matrix.shape
+    indices = np.random.choice(rows * cols, size=n, replace=False)
+    row_indices = indices // cols
+    col_indices = indices % cols
+    return list(zip(row_indices, col_indices))
+
+
+def generate_hidden_matrix(n_x, sparsity, lower_bound=-1, upper_bound=1):
     """
     This function generates a sparse matrix of weights for the reservoir (W).
     :param n_x: The number of neurons in the reservoir.
@@ -39,20 +55,115 @@ def generate_sparse_matrix(n_x, sparsity, lower_bound=-1, upper_bound=1):
     w = np.zeros((n_x, n_x))
     # Calculate the number of non-zero weights in the reservoir.
     num_non_zero = int(np.round(sparsity * n_x * n_x))
+    print(f'The number of non-zero weights in the reservoir is {num_non_zero:,}')
+
     # Generate a random list of indices for the non-zero weights in the reservoir.
     # The indices are generated from a uniform distribution.
     # The indices are generated from 0 to n_x - 1.
     # The indices are generated without replacement.
-    indices = np.random.choice(np.arange(n_x * n_x), num_non_zero, replace=False)
+    # The matrix is 2d, so we need to generate two lists of indices.
+
+    # we need to generate two lists of indices, one for the x-axis and one for the y-axis.
+    # The indices are generated from 0 to n_x - 1.
+    # indices_x = np.random.choice(np.arange(n_x*n_x), num_non_zero, replace=False)
+    # indices_y = np.random.choice(np.arange(n_x*n_x), num_non_zero, replace=False)
+    indices = generate_random_indices(w, num_non_zero)
+
     # Generate a random list of weights for the non-zero weights in the reservoir.
     # The weights are generated from a uniform distribution.
     # The weights are generated from -1 to 1.
     # The weights are generated from a uniform distribution.
     weights = np.random.uniform(lower_bound, upper_bound, num_non_zero)
+
+    # print(len(indices_x))
+    # print(len(indices_y))
+    # print(len(weights))
     # Assign the weights to the indices in the reservoir.
-    w.ravel()[indices] = weights
+    for i in range(num_non_zero):
+        x = indices[i][0]
+        y = indices[i][1]
+        if x > n_x or y > n_x:
+            print(f'x is {x} and y is {y}')
+        w[x][y] = weights[i]
+    # w.ravel()[indices] = weights
     # Return the reservoir weights.
     return w
+
+
+def generate_sparse_weights(n_x, n_u, sparsity, lower_bound=-1, upper_bound=1):
+    """
+    This function is responsible for generating the input weights for the ESN model.
+    :param n_x: The number of neurons in the reservoir.
+    :param n_u: The number of input neurons, corresponding to the number of channels in the input data.
+    :param sparsity: The percentage of neurons that are connected to other neurons.
+    :param lower_bound: The lower bound for the uniform distribution the non-zero weights are generated from.
+    :param upper_bound: The upper bound for the uniform distribution the non-zero weights are generated from.
+    :return: A sparse matrix containing weights for the input (w_in) of size n_x by n_u.
+    """
+    # Create a matrix of zeros of size n_x by n_u.
+    w_in = np.zeros((n_x, n_u))
+
+    # Calculate the number of non-zero weights in the reservoir dictated by the sparsity.
+    num_non_zero = int(np.round(sparsity * n_x * n_u))
+
+    # Generate a random list of indices for the non-zero weights in the reservoir dictated by the sparsity.
+    indices = np.random.choice(np.arange(n_x * n_u), num_non_zero, replace=False)
+
+    # Generate a random list of weights for the non-zero weights in the reservoir.
+    weights = np.random.uniform(lower_bound, upper_bound, num_non_zero)
+
+    # Assign the weights to the indices in the reservoir.
+    w_in.ravel()[indices] = weights
+
+    # Return the input weights.
+    return w_in
+
+
+def generate_neurons(n_x):
+    """
+    This function is responsible for generating the initial state of the reservoir.
+    :param n_x: The number of neurons in the reservoir.
+    :return: The initial state of the reservoir.
+    """
+    # Generate a random vector of size n_x.
+    x = np.random.uniform(-1, 1, n_x)
+    # Return the initial state of the reservoir.
+    return x
+
+
+def update_state(x, u, w_in, w, alpha):
+    """
+    This function is responsible for updating the state x(n) of the reservoir.
+    The update of a state involves 2 steps of equations: [;] indicates vertical concatenation.
+    First, the update of the state is calculated by the equation: x_update(n) = tanh(w_in*[1;u(n)] + w*x(n-1))
+    Second, the state is now calculated by the equation: x(n) = (1-alpha)*x(n-1) + alpha*x_update(n)
+    :param x: The state of the reservoir at time n-1.
+    :param u: The input data at time n, corresponding to a reading from an EEG channel.
+    :param w_in: The input weights of the reservoir.
+    :param w: The hidden weights of the reservoir.
+    :param alpha: The leak rate of the reservoir.
+    :return: The state of the reservoir at time n.
+    """
+    # Calculate the update of the state.
+    x_update = np.tanh(np.dot(w_in, np.vstack((1, u))) + np.dot(w, x))
+    # Calculate the state.
+    x = (1 - alpha) * x + alpha * x_update  # muahahahahaha i think it's gonnnna work!!!! :D
+    # Return the state.
+    return x
+
+
+def train_state(x, u, w_in, w, alpha):
+    """
+    This function is responsible for training the reservoir of the input data and updating the weights using all the
+    traning data.
+    :param x: The activations of the reservoir.
+    :param u: The input data.
+    :param w_in: The input weights of the reservoir.
+    :param w: The hidden weights of the reservoir.
+    :param alpha: The leak rate of the reservoir.
+    :return: The updated activations of the reservoir.
+    """
+    pass
 
 
 def main():
@@ -62,24 +173,38 @@ def main():
     slightly problematic.
     """
     Nx = 500
+    Nu = 2
+    Ny = 10
+    alpha = 0.3
     sparseness = 0.1
     little_bound = -1
     big_bound = 1
-    #rescale_factor = 0.4
+    # rescale_factor = 0.4
+
+    # create the input weight matrix w_in
+    w_in = generate_sparse_weights(Nx, Nu, sparseness, little_bound, big_bound)
 
     # Generate the sparse matrix of weights for the reservoir (W).
-    sparse_matrix = generate_sparse_matrix(Nx, sparseness, little_bound, big_bound)
-    #sparse_matrix *= rescale_factor
-    # for row in sparse_matrix: # Looks sparsy to me
-    #     print(row)
-
+    sparse_matrix = generate_hidden_matrix(Nx, sparseness, little_bound, big_bound)
     # Calculate the spectral radius of the reservoir weights.
     spectral_radius = get_spectral_radius(sparse_matrix)
-    print(spectral_radius)
-    sparse_matrix /= spectral_radius
-    print(get_spectral_radius(sparse_matrix))
+    # rescale the weights
+    w = sparse_matrix / spectral_radius
 
-    # TODO: go onto the input scaling part of building the reservoir for the ESN model.
+    # Generate the initial state of the reservoir.
+    x = generate_neurons(Nx)
+
+    # Generate the weights for the output layer of the ESN model.
+    w_out = generate_sparse_weights(Nx, Ny, sparseness, little_bound, big_bound)
+
+    # Generate the units for the output layer of the ESN model.
+    y = generate_neurons(Ny)
+
+    # TODO 1: go onto the input scaling part of building the reservoir for the ESN model.
+    # TODO 2: Compare the two methods for generating sparse matrices, and evaluate their spectral radi.
+    # TODO 3: Implement the necessary achitecture for the readout layer of the ESN model. (i.e. the output weights)
+    # TODO 4: Implement the necessary architecture for the training of the ESN model maybe in a class though.
+    # TODO 5: Investigate the reservoir dynamics of the ESN model using the training data.
 
 
 if __name__ == '__main__':
